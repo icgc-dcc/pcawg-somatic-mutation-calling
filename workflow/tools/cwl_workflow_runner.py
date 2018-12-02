f32d1e9a72d15929d6474b87ec200b75139a5698
Junjun Zhang <junjun.ca@gmail.com>
2018-12-01 21:22:26 -0500

    now it's testable

--- workflow/tools/cwl_workflow_runner.py
+++ workflow/tools/cwl_workflow_runner.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+import os
+import re
+import json
+import yaml
+import subprocess
+
+
+task_dict = json.loads(sys.argv[1])
+
+cwl_source = task_dict['cwl_source']
+cwl_entry = task_dict['cwl_entry']
+
+m = re.match('(.+\/.+\/(.+)):(.+)', cwl_source)
+
+if m:
+    git_url = 'https://%s' % m.group(1)
+    repo_name = m.group(2)
+    release_tag = m.group(3)
+else:
+    exit('CWL source Git repo incorrect: %s' % cwl_source)
+
+command = 'git clone %s; cd %s; git checkout %s' % (git_url, repo_name, release_tag)
+
+subprocess.call(command, shell=True)
+
+input_template = subprocess.check_output(["cwltool",
+                                          "--make-template",
+                                          os.path.join(repo_name, cwl_entry)
+                                          ])
+
+input_json = yaml.load(input_template)
+
+exit({
+        "task_dict": task_dict,
+        "input_json_template": input_json
+    })
--- workflow/tools/dockstore_tool_runner.py
+++ workflow/tools/dockstore_tool_runner.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import json
+import yaml
+import subprocess
+
+
+task_dict = json.loads(sys.argv[1])
+
+input_template = subprocess.check_output(["cwltool",
+                                          "--make-template",
+                                          task_dict.get("dockstore_tool")
+                                          ])
+
+input_json = yaml.load(input_template)
+
+exit({
+        "task_dict": task_dict,
+        "input_json_template": input_json
+    })
