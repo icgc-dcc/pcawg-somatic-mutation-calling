736d2f3f7381ad5275c6891bbbc95ae789f85d71
baminou <brice.aminou@gmail.com>
2018-12-19 12:19:37 -0500

    fixes

--- workflow/tools/download_file.py
+++ workflow/tools/download_file.py
@@ -8,19 +8,19 @@ import shutil
 
 task_dict = json.loads(sys.argv[1])
 task_input = task_dict['input']
-file_name = task_input.get('file_name')
+file_path = task_input.get('file_path')
 
-output_file = file_name
+output_file = file_path
 
-if str(file_name).startswith('song://'):
-    object_id = str(file_name).split('/')[4]
-    profile = str(file_name).split('/')[2]
+if str(file_path).startswith('song://'):
+    object_id = str(file_path).split('/')[4]
+    profile = str(file_path).split('/')[2]
+
+    if not profile in ['collab','aws']: raise Exception("Provide a profile in collab or aws. %s" % (profile))
 
     outdir = os.path.join(os.getcwd(),'outdir')
-    if os.path.exists(outdir):  # remove  if exist
-        shutil.rmtree(outdir)
 
-    os.makedirs(outdir)
+    os.makedirs(outdir, exist_ok=True)
 
     score_container = "overture/score:1.5.0"
     subprocess.check_output(['docker', 'pull', score_container])
@@ -35,9 +35,10 @@ if str(file_name).startswith('song://'):
     for f in os.listdir(outdir):
         if f.endswith('.bam'):
             output_file = os.path.join(outdir,f)
-        else:
-            output_file = None
+            break
 
+if not (output_file.endswith('.bam') and os.path.isfile(output_file)):
+    exit('Input BAM not ready!')
 
 with open('output.json', 'w') as f:
     f.write(json.dumps({'file_path':output_file}, indent=2))
